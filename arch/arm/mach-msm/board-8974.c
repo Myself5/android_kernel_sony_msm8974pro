ce01219afdcef7395bdde675d3b61db3efc59110
Myself5 <myself5@carbonrom.org>
2014-05-23 00:22:05 -0400

    Implement kexec-hardboot
    "Allows hard booting (i.e., with a full hardware reboot) to a kernel
    previously loaded in memory by kexec.  This works around the problem of
    soft-booted kernel hangs due to improper device shutdown and/or
    reinitialization."
    More info in /arch/arm/Kconfig.
    
    Original author: Mike Kasick <mike@kasick.org>
    
    Vojtech Bocek <vbocek@gmail.com>:
      I've ported it to hammerhead, it is based of my mako/grouper port, which is
      based of Asus TF201 patch ported by Jens Andersen <jens.andersen@gmail.com>
    
      Hammerhead is the first device I've ported this patch to which uses
      device tree binary instead of atags. It is also rather picky - its
      bootloader erases first 128MB of RAM on reboot, so following hacks had
      to be applied:
      * Copy dtb to location passed in r2 before jumping to kexecd' kernel,
        the kernel would just freeze when I left dtb above 128MB of RAM.
      * Copy kexecd' kernel to location in the first 128MB before jumping to it,
        the decompressor operates very slowly if the kernel is above the cached
        area, copying it lower and decompressing it afterwards is much faster.
    
      Same as my mako/flo patches, the guest kernel does not require any
      modifications, kexec-hardboot is equivalent to standard kexec.
    
      To support dtb, following commit was backported from the mainline:
        commit 4cabd1d9625c7d88acd143f4021fbef75394f154
        Author: Matthew Leach <matthew.leach@arm.com>
        Date:   Fri Sep 21 18:56:02 2012 +0100
    
          ARM: 7539/1: kexec: scan for dtb magic in segments
    
          This patch allows a dtb to be passed to a new kernel using the kexec
          mechinism.
    
          When loading segments from userspace, scan each segment's first four
          bytes for the dtb magic. If this is found set the kexec_boot_atags
          parameter to the relocate_kernel code to the phyical address of this
          segment.
    
      and fixes of this commit:
        c564df4db85aac8d1d65a56176a0a25f46138064
        2456f44dd7a9aaffc2cd21a13f78198b3d94da08
    
    ayysir <dresadd09691@gmail.com>:
      kexec: use mem_text_write_kernel_word to set reboot_code_buffer args
      in order to avoid protection faults (writes to read-only
      kernel memory) when CONFIG_STRICT_MEMORY_RWX is enabled.
    
    Conflicts:
    	arch/arm/mach-msm/board-8974.c
    	arch/arm/mach-msm/include/mach/memory.h

--- arch/arm/mach-msm/board-8974.c
+++ arch/arm/mach-msm/board-8974.c
@@ -57,6 +57,11 @@
 #include "board-8974-wifi.h"
 #include "board-8974-console.h"
 
+#ifdef CONFIG_KEXEC_HARDBOOT
+#include <linux/memblock.h>
+#include <asm/setup.h>
+#endif
+
 #ifdef CONFIG_RAMDUMP_TAGS
 static struct resource rdtags_resources[] = {
 	[0] = {
@@ -186,6 +191,27 @@ static struct platform_device ram_console_device = {
 
 void __init msm_8974_reserve(void)
 {
+#ifdef CONFIG_KEXEC_HARDBOOT
+	// Reserve space for hardboot page - just after ram_console,
+	// at the start of second memory bank
+	int ret;
+	phys_addr_t start;
+	struct membank* bank;
+
+	if (meminfo.nr_banks < 2) {
+		pr_err("%s: not enough membank\n", __func__);
+		return;
+	}
+
+	bank = &meminfo.bank[1];
+	start = bank->start + SZ_1M + MSM_PERSISTENT_RAM_SIZE;
+	ret = memblock_remove(start, SZ_1M);
+	if(!ret)
+		pr_info("Hardboot page reserved at 0x%X\n", start);
+	else
+		pr_err("Failed to reserve space for hardboot page at 0x%X!\n", start);
+#endif
+
 #if defined(CONFIG_RAMDUMP_TAGS) || defined(CONFIG_CRASH_LAST_LOGS)
 	reserve_debug_memory();
 #endif
