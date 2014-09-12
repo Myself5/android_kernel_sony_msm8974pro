f72735b6cfffe655bb363bb285a0a0b159ae63b7
Myself5 <thchod@gmail.com>
2014-09-12 08:01:15 +0200

    test to keep touch on #2

--- drivers/input/touchscreen/clearpad_core.c
+++ drivers/input/touchscreen/clearpad_core.c
@@ -3112,7 +3112,7 @@ enable:
 	rc = request_threaded_irq(this->irq,
 				synaptics_clearpad_hard_handler,
 				synaptics_clearpad_threaded_handler,
-				IRQF_TRIGGER_FALLING | IRQF_ONESHOT,
+				IRQF_TRIGGER_FALLING | IRQF_ONESHOT | IRQF_NO_SUSPEND,
 				this->pdev->dev.driver->name,
 				&this->pdev->dev);
 	if (rc) {
